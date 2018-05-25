e6b6b66d76280bcfa8067c26e946e1c32a95bb81
deusyvan <deusyvan@gmail.com>
2018-05-24 21:48:25 -0300

    Fazendo o input escolher arquivo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <form method="POST" enctype="multipart/form-data">
 
-	<input type="text" name="nome"/><br/><br/>
+	<input type="file" name="arquivo"/><br/><br/>
 	
 	<input type="submit" value="Enviar"/>
 
