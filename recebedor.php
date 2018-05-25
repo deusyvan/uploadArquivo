41fc8e278c4b97025095b8936fe3fbe26d749e7a
deusyvan <deusyvan@gmail.com>
2018-05-24 23:33:37 -0300

    Pega o arquivo e envia pra pasta arquivos

--- recebedor.php
+++ recebedor.php
@@ -3,7 +3,8 @@
 $arquivo = $_FILES['arquivo'];
 
 if(isset($arquivo['tmp_name']) && !empty($arquivo['tmp_name'])){
-    
+    move_uploaded_file($arquivo['tmp_name'], 'arquivos/'.$arquivo['name']);
+    echo 'Arquivo enviado com sucesso!';
 }
 
 ?>
